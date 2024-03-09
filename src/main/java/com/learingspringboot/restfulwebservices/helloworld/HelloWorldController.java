82dc11dd891fa96eac3c42cb9a5020dfd99a2030
Himanshu Tiwari <realhimanshutiwari@gmail.com>
2024-03-09 23:19:34 +0530

    Internationalization i18n

--- src/main/java/com/learingspringboot/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/learingspringboot/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,15 +1,24 @@
 package com.learingspringboot.restfulwebservices.helloworld;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 // REST API
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
 
     // when do to localhost:8080/hello-world
     // return "Hello World"
@@ -34,5 +43,21 @@ public class HelloWorldController {
         return new HelloWorldBean(
                 String.format("Hello World, %s", name));
     }
+
+    @GetMapping( "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale =LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",
+                null, "Default Message", locale);
+
+        //return "Hello Himanshu V2";
+    }
+
+    //good.morning.message=Good Morning
+
+    //Example:
+    //'en' - English(Good Morning)
+    //'nl' - Dutch(Goedemorgen)
+    //'fr' - French(Bonjour)
 
 }
