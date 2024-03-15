a0700d7b39326f8f6e65b4308a44a012fc787cbd
Himanshu Tiwari <realhimanshutiwari@gmail.com>
2024-03-15 21:50:10 +0530

    Filtering: Static and Dynamic

--- src/main/java/com/learingspringboot/restfulwebservices/user/User.java
+++ src/main/java/com/learingspringboot/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.learingspringboot.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,8 +10,10 @@ public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name should have atleast 2 characters")
+    @JsonProperty("user_name") // Changes field Name (name -> user_name)
     private String name;
 
+    @JsonProperty("birth_date")
     @Past(message = "Birth Date should be in the past")
     private LocalDate birthDate;
 
