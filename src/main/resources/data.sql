e23c8add0f743afa55a1f01e8f89b0ad7b21ddaa
Himanshu Tiwari <realhimanshutiwari@gmail.com>
2024-03-17 13:56:44 +0530

    Creating Post Entity and Database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,15 @@ values (10003,'Vaibhaw Chaubey',current_date());
 
 insert into user_details(id,name,birth_date)
 values (10004,'Sonu Kumar',current_date());
+
+insert into post(id,description,user_id)
+values (20001,'I want to learn Java',10001);
+
+insert into post(id,description,user_id)
+values (20002,'I want to learn Spring Boot',10001);
+
+insert into post(id,description,user_id)
+values (20003,'I want to learn AWS',10002);
+
+insert into post(id,description,user_id)
+values (20004,'I want to learn DevOps',10002);
